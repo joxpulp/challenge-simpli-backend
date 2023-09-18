cf1f16ceabae6a00210f07fa60e9fec675df5342
JOSUE <joxpulp@gmail.com>
2023-09-18 13:37:03 -0300

    fix:

--- src/services/redis/redis.ts
+++ src/services/redis/redis.ts
@@ -2,6 +2,7 @@ import { createClient } from 'redis';
 import { CONFIG } from '../../utils/config/config';
 
 export const client = createClient({
+  username: 'default',
   password: CONFIG.REDIS_PWD,
   socket: {
     host: CONFIG.REDIS_HOST,
